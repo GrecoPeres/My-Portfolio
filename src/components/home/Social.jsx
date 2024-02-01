5d9ad4df44d2272e326bd516af9a7930b4bd47e2
Greco Fernando M.P <100162697+GrecoPeres@users.noreply.github.com>
2024-02-01 15:02:06 -0300

    My First Portfolio in React

---
+++ src/components/home/Social.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const Social = () => {
+  return (
+    <div className="home__social">
+        <a href="https://github.com/GrecoPeres" className="home__social-icon">
+            <i class="uil uil-github"></i>
+        </a>
+
+        <a href="https://www.linkedin.com/in/greco-fernando-873038207/" className="home__social-icon">
+            <i class="uil uil-linkedin"></i>
+        </a>
+
+        <a href="https://www.instagram.com/grecoperes/" className="home__social-icon">
+            <i class="uil uil-instagram"></i>
+        </a>
+    </div>
+  )
+}
+
+export default Social
