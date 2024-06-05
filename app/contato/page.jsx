09b8650f5a81928a993ecb6b5a646ac7cf90fa98
Greco Fernando M.P <100162697+GrecoPeres@users.noreply.github.com>
2024-06-05 17:31:20 -0300

    feat: I finished the lasted section - Contact

--- app/contato/page.jsx
+++ app/contato/page.jsx
@@ -33,21 +33,54 @@ const Contato = () => {
     >
       <div className="container mx-auto">
         <div className="flex flex-col xl:flex-row gap-[30px]">
-          {/* formulario de contato */}
-          <div className="xl:h-[54%] order-2 xl:order-none">
+          {/* formulario */}
+          <div className="xl:w-[54%] order-2 xl:order-none">
             <form className="flex flex-col gap-6 p-10 bg-[#27272c] rounded-xl">
               <h3 className="text-4xl text-white"> I turn coffee into <span className="text-accent"> &lt;/code&gt; </span> </h3>
               <p className="text-white/60">Lorem ipsum dolor sit amet consectetur, adipisicing elit.</p>
               {/* input */}
               <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
                 <Input type="nome" placeholder="Nome completo" />
-                <Input type="email" placeholder="E-mail" />
                 <Input type="telefone" placeholder="Telefone" />
+                <Input type="email" placeholder="E-mail" />
               </div>
+              {/* select */}
+              <Select>
+                <SelectTrigger className="w-full">
+                  <SelectValue placeholder="Selecione o serviço" />
+                </SelectTrigger>
+                <SelectContent>
+                  <SelectGroup>
+                    <SelectLabel>Selecione o serviço</SelectLabel>
+                    <SelectItem value="dev-web">Desenvolvimento Web</SelectItem>
+                    <SelectItem value="ui-ux">UI/UX Design</SelectItem>
+                    <SelectItem value="dev-mob">Desenvolvimento Mobile</SelectItem>
+                  </SelectGroup>
+                </SelectContent>
+              </Select>
+              {/* texto */}
+              <Textarea className="h-[200px]" placeholder="Digite sua mensagem aqui." />
+              <Button size="md" className="max-w-40">Enviar</Button>
             </form>
           </div>
           {/* informações */}
-          <div className="flex-1 flex items-center xl:justify-end order-1 xl:order-none mb-8 xl:mb-0">info</div>
+          <div className="flex-1 flex items-center xl:justify-end order-1 xl:order-none mb-8 xl:mb-0">
+            <ul className="flex flex-col gap-10">
+              {info.map((item, index) => {
+                return (
+                  <li key={index} className="flex items-center gap-6">
+                    <div className="w-[52px] h-[52px] xl:w-[72px] xl:h-[72px] bg-[#27272c] text-accent rounded-md flex items-center justify-center">
+                      <div className="text-[28px]">{item.icon}</div>
+                    </div>
+                    <div className="flex-1">
+                      <p className="text-white/60">{item.titulo}</p>
+                      <h3 className="text-xl">{item.descricao}</h3>
+                    </div>
+                  </li>
+                );
+              })}
+            </ul>
+          </div>
         </div>
       </div>
     </motion.section>
