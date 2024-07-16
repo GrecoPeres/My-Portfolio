c474c7e04fd7c3bd5a2e5ae1ffd33146776eb466
Greco Fernando M.P <100162697+GrecoPeres@users.noreply.github.com>
2024-07-16 14:31:50 -0300

    fix: I fixing the div grid in tabNavigator do About

--- app/sobre/page.jsx
+++ app/sobre/page.jsx
@@ -262,7 +262,7 @@ const Sobre = () => {
               <div className='flex flex-col gap-[30px]'>
                 <h3 className='text-4xl font-bold'>{about.titulo}</h3>
                 <p className='max-w-[600px] text-white/60 mx-auto xl:mx-0'>{about.descricao}</p>
-                <ul className='grid grid-cols-1 xl:grid-cols-2 gap-y-6 max-w-[620px] mx-auto xl:mx-0'>
+                <ul className='grid grid-cols-1 xl:grid-cols-1 gap-y-6 max-w-[620px] mx-auto xl:mx-0'>
                   {about.info.map((item, index) =>{
                     return (
                       <li key={index} className='flex item-center justify-center xl:justify-start gap-4'>
